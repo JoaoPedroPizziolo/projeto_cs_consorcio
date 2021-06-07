3e3f6a9f0f5a54ed4068aa83205d98ec55c5aa4e
JoaoPedroPizziolo <84928663+JoaoPedroPizziolo@users.noreply.github.com>
2021-06-07 17:47:58 -0300

    Add files via upload

---
+++ projeto_cs_consorcio/Exe12_pag30_24/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Exe12_pag30_24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtvalor = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.rdbA = new System.Windows.Forms.RadioButton();
+            this.rdbB = new System.Windows.Forms.RadioButton();
+            this.rdbC = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btncalcule = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.Location = new System.Drawing.Point(12, 28);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(141, 20);
+            this.txtvalor.TabIndex = 0;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(13, 12);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(140, 13);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Digite o valor do emprestimo";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(12, 55);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(57, 26);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "Selecione \r\num plano";
+            this.lbl2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(75, 68);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(60, 13);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "Prestaçoes";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(145, 68);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(32, 13);
+            this.lbl4.TabIndex = 4;
+            this.lbl4.Text = "Juros";
+            // 
+            // rdbA
+            // 
+            this.rdbA.AutoSize = true;
+            this.rdbA.Location = new System.Drawing.Point(15, 85);
+            this.rdbA.Name = "rdbA";
+            this.rdbA.Size = new System.Drawing.Size(32, 17);
+            this.rdbA.TabIndex = 5;
+            this.rdbA.TabStop = true;
+            this.rdbA.Text = "A";
+            this.rdbA.UseVisualStyleBackColor = true;
+            // 
+            // rdbB
+            // 
+            this.rdbB.AutoSize = true;
+            this.rdbB.Location = new System.Drawing.Point(16, 109);
+            this.rdbB.Name = "rdbB";
+            this.rdbB.Size = new System.Drawing.Size(32, 17);
+            this.rdbB.TabIndex = 6;
+            this.rdbB.TabStop = true;
+            this.rdbB.Text = "B";
+            this.rdbB.UseVisualStyleBackColor = true;
+            // 
+            // rdbC
+            // 
+            this.rdbC.AutoSize = true;
+            this.rdbC.Location = new System.Drawing.Point(16, 133);
+            this.rdbC.Name = "rdbC";
+            this.rdbC.Size = new System.Drawing.Size(32, 17);
+            this.rdbC.TabIndex = 7;
+            this.rdbC.TabStop = true;
+            this.rdbC.Text = "C";
+            this.rdbC.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "100";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "144";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "180";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "1,8%";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(142, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "2,5%";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(139, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "3,5%";
+            // 
+            // btncalcule
+            // 
+            this.btncalcule.Location = new System.Drawing.Point(12, 157);
+            this.btncalcule.Name = "btncalcule";
+            this.btncalcule.Size = new System.Drawing.Size(157, 23);
+            this.btncalcule.TabIndex = 14;
+            this.btncalcule.Text = "Calcule";
+            this.btncalcule.UseVisualStyleBackColor = true;
+            this.btncalcule.Click += new System.EventHandler(this.btncalcule_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(209, 207);
+            this.Controls.Add(this.btncalcule);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rdbC);
+            this.Controls.Add(this.rdbB);
+            this.Controls.Add(this.rdbA);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txtvalor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtvalor;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.RadioButton rdbA;
+        private System.Windows.Forms.RadioButton rdbB;
+        private System.Windows.Forms.RadioButton rdbC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btncalcule;
+    }
+}
+
---
+++ projeto_cs_consorcio/Exe12_pag30_24/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exe12_pag30_24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalcule_Click(object sender, EventArgs e)
+        {
+            double montante, montante_final, capital, novo_valor, parcelas, juros;
+
+            capital = double.Parse(txtvalor.Text);
+
+            if (rdbA.Checked)
+            {
+                montante = capital * Math.Pow((1 + 0.018), 100);
+                juros = montante - capital;
+                parcelas = capital * 0.018;
+
+                MessageBox.Show("seu juros sera de: " + juros.ToString("C") + "O valor total sera de: " + montante.ToString("C") + "o valor de cada parcela sera: " + parcelas.ToString("C");
+            }
+            else if (rdbB.Checked)
+            {
+                montante = capital * Math.Pow((1 + 0.025), 100);
+                juros = montante - capital;
+                parcelas = capital * 0.025;
+
+                MessageBox.Show("seu juros sera de: " + juros.ToString("C") + "O valor total sera de: " + montante.ToString("C") + "o valor de cada parcela sera: " + parcelas.ToString("C");
+            }
+            else if (rdbC.Checked)
+            {
+                montante = capital * Math.Pow((1 + 0.035), 100);
+                juros = montante - capital;
+                parcelas = capital * 0.035;
+
+                MessageBox.Show("seu juros sera de: " + juros.ToString("C") + "O valor total sera de: " + montante.ToString("C") + "o valor de cada parcela sera: " + parcelas.ToString("C");
+            }
+        }
+    }
+}
